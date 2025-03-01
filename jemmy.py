733ac8a60f9d4cae43ca7c162630b0230a3cbe9b
Krobix <brendon.shaw8921@gmail.com>
2025-03-01 13:55:43 -0500

    that should fix it please

--- jemmy.py
+++ jemmy.py
@@ -73,7 +73,7 @@ class Jemmy(discord.Client):
                 prompt = create_prompt(messages)
                 out = llm(prompt=prompt, max_tokens=256, temperature=TEMP, stop=["<|endmessage"])
                 pl = len(prompt)
-                outs = out["choices"][0]
+                outs = out["choices"][0]["text"]
                 await msg.reply(outs)
 
 intents = discord.Intents.default()
