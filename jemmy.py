15aa7840455ccf109ce2e9c18581992212cd7b1e
Krobix <brendon.shaw8921@gmail.com>
2025-03-07 15:22:00 -0500

    i hate this

--- jemmy.py
+++ jemmy.py
@@ -3,8 +3,8 @@ import discord
 import os, random, threading, asyncio, secrets, time, copy
 
 MODEL_PATH = "/xb/llms/mistral-7b-v0.1.Q5_K_M.gguf"
-TEMP = 1.1
-REP_PENALTY=1.3
+TEMP = 1.0
+REP_PENALTY=1.18
 llm = Llama(MODEL_PATH, n_ctx=2048, n_threads=6, n_threads_batch=12)
 
 replace_channel_names = []
